d674105c4ceabd7ed0933de9bbcb09e2bb8df8dc
Alex Walker <walkeralex81@gmail.com>
2023-09-19 20:22:47 +0100

    0

---
+++ app.js
@@ -0,0 +1,7 @@
+let user = prompt("What is your name?");
+console.log("Their user's name is " + user);
+
+let colour = prompt("What is your favorite anthem?");
+console.log("Your favourite anthem is " + colour);
+
+alert("Hi there, " + user + ". Your favourite anthem is " + colour);
